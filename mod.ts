d38b4eaaa5212694e028d04116db0b6ea85ac0a9
azulamb <1127785+azulamb@users.noreply.github.com>
2024-12-11 09:19:04 +0900

    Update nuget.

--- mod.ts
+++ mod.ts
@@ -1,2 +1,3 @@
 export const VERSION = '0.0.1';
+export * from './src/version.ts';
 export * from './src/weapn_app.ts';
--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const Microsoft_Web_WebView2 = '1.0.1661.34';
-export const Microsoft_Windows_ImplementationLibrary = '1.0.230202.1';
+export const Microsoft_Web_WebView2 = '1.0.2903.40';
+export const Microsoft_Windows_ImplementationLibrary = '1.0.240803.1';
